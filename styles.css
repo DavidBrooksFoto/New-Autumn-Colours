cf7798e40bfacd318cce5345e280eef972e9e8f7
David Peter Brooks <62650271+DavidBrooksFoto@users.noreply.github.com>
2023-11-13 21:11:46 +0100

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,12 @@
+#grid {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(10, 1fr);
+    gap: 2px;
+}
+
+.cell {
+    width: 50px;
+    height: 50px;
+    background-color: green; /* Initial color, will be overridden by JS */
+}
