f345805654d6d9a9825f16c16059304c34f89595
David Peter Brooks <62650271+DavidBrooksFoto@users.noreply.github.com>
2023-11-14 20:53:22 +0100

    Updated 14 Nov

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-#grid {
-    display: grid;
-    grid-template-columns: repeat(10, 1fr);
-    grid-template-rows: repeat(10, 1fr);
-    gap: 2px;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.getElementById('grid');
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
 
-.cell {
-    width: 50px;
-    height: 50px;
-    background-color: green; /* Initial color, will be overridden by JS */
+    const autumnColors = ['#FF7F50', '#FFD700', '#FF8C00', '#ADFF2F', '#DEB887'];
+
+    setInterval(() => {
+        document.querySelectorAll('.cell').forEach(cell => {
+            const randomColor = autumnColors[Math.floor(Math.random() * autumnColors.length)];
+            cell.style.backgroundColor = randomColor;
+        });
+    }, getRandomInterval());
+});
+
+function getRandomInterval() {
+    return Math.random() * (4000 - 500) + 500;
 }
