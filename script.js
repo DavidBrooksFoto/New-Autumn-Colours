de31a198ec755aba00ed105124a846bce3bd43af
David Peter Brooks <62650271+DavidBrooksFoto@users.noreply.github.com>
2023-11-19 10:26:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
         grid.appendChild(cell);
     }
 
-    const autumnColors = ['#FF7F50', '#FFD700', '#FF8C00', '#ADFF2F', '#DEB887'];
+    const autumnColors = [
+        '#FF7F50', '#FFD700', '#FF8C00', '#ADFF2F', '#DEB887',
+        // ... add more colors to reach at least 100 unique colors
+        '#D2691E', '#8B0000', '#FF4500', '#FFA07A', '#FF6347',
+        '#FFA500', '#FFDAB9', '#E9967A', '#FA8072', '#FF69B4',
+        '#CD5C5C', '#F08080', '#B22222', '#FF0000', '#8B4513',
+        '#A0522D', '#D2B48C', '#DEB887', '#F4A460', '#DAA520',
+        // Add more colors as needed
+    ];
 
     setInterval(() => {
         document.querySelectorAll('.cell').forEach(cell => {
